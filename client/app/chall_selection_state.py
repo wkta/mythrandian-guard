81d638f9c4555ec1dc21fcfc9d3cbbca32cc99ed
wkta <tiwaszko@gmail.com>
2022-01-29 19:29:12 +0100

    Mission states(add model)+skeleton for players farming

--- client/app/chall_selection_state.py
+++ client/app/chall_selection_state.py
@@ -1,17 +1,19 @@
 import random
-
+import time
+import pkatagames_sdk as katasdk
 import game_defs
-import pkatagames_sdk
 from app.AvatarModel import AvatarModel
-from app.BelongingsModel import Artifact
-from game_defs import GameStates
-from pkatagames_sdk.engine import BaseGameState, EngineEvTypes, EventReceiver, EventManager, import_pygame, CgmEvent
-from pkatagames_sdk.ext_gui import Button
 from app.AvatarView import AvatarView
+from app.BelongingsModel import Artifact
+from app.Loot import Loot
+from game_defs import GameStates
+from game_events import MyEvTypes
+from pkatagames_sdk.engine import BaseGameState, EngineEvTypes, EventReceiver, EventManager, CgmEvent, CogObject
+from pkatagames_sdk.ext_gui import Button
 
 
-kataen = pkatagames_sdk.engine
-pygame = import_pygame()
+kataen = katasdk.engine
+pygame = katasdk.import_pygame()
 
 
 # -- to be bound to GUI buttons --
@@ -35,9 +37,63 @@ def click_gen_element():
     print(str(tmp))
 
 
-class ChallSelectionView(EventReceiver):
+class ChallSelectionModel(CogObject):  # it holds the state of missions
+    """
+    3 missions at the same time
+    """
+
     def __init__(self):
+        super().__init__()
+        self._ongoing_missions = {
+            1: 0,
+            2: 0,
+            3: 0
+        }
+        self._difficulties = {
+            1: 1.1,
+            2: 1.5,
+            3: 2.25
+        }
+
+    def start_mission(self, index):
+        print('mission {} starts!'.format(index))
+        self._ongoing_missions[index] = 1
+        self.pev(MyEvTypes.MissionStarts, t=time.time(), idx=index)
+
+    def flag_mission_done(self, index):
+        print('mission {} done!'.format(index))
+        self._ongoing_missions[index] = -1
+        self.pev(MyEvTypes.MissionEnds, idx=index)
+
+    def is_m_open(self, index):
+        return 0 == self._ongoing_missions[index]
+
+    def is_m_locked(self, index):
+        return 1 == self._ongoing_missions[index]
+
+    def is_m_over(self, index):
+        return -1 == self._ongoing_missions[index]
+
+    def claim_reward(self, index):
+        if not self.is_m_over(index):
+            raise Exception
+        self._ongoing_missions[index] = 0  # reset state
+
+        obj_loot = Loot.gen_random(self._difficulties[index])
+        self.pev(MyEvTypes.LootReady, idx=index, obj=obj_loot)
+        print('sendin loot!')
+
+
+class ChallSelectionView(EventReceiver):
+
+    def __init__(self, ref_mod):
         super().__init__(self)
+
+        # small red squares to tell the mission is ongoing
+        self._squares = dict()
+
+        self._model = ref_mod
+
         self._bg_color = 'antiquewhite3'  # or smth like (255, 50, 60) if we choose the red, green, blue format
         ft = pygame.font.Font(None, 19)
         # self.img = ft.render('press mouse button to change state', True, (0, 0, 0))
@@ -48,41 +104,92 @@ class ChallSelectionView(EventReceiver):
         # - set all buttons
         fixed_size = (150, 48)
         bpos = list()
-        for i in range(0, 5):
-            bpos.append((228 * i, self._scr_size[1]-64))
+        for index in range(0, 5):
+            bpos.append((228 * index, self._scr_size[1] - 50))
 
+        cls = self.__class__
         self._buttons = {
+            'm1': Button(pos=cls.position_m_square(1), size=fixed_size, label='mission1'),
+            'm2': Button(pos=cls.position_m_square(2), size=fixed_size, label='mission2'),
+            'm3': Button(pos=cls.position_m_square(3), size=fixed_size, label='mission3'),
+
             'shop': Button(pos=bpos[1], size=fixed_size, label='go to the shop'),
             'fight': Button(pos=bpos[2], size=fixed_size, label='take a fight'),
             'reward': Button(pos=bpos[3], size=fixed_size, label='gen reward'),
         }
 
+        def effetm1():
+            if not self._model.is_m_locked(1):
+                if self._model.is_m_over(1):
+                    self._model.claim_reward(1)
+                else:  # therefore open
+                    self._model.start_mission(1)
+
+        self._buttons['m1'].callback = effetm1
+
         self._buttons['shop'].callback = click_shop
         self._buttons['fight'].callback = click_fight
         self._buttons['reward'].callback = click_gen_element
+
+    @staticmethod
+    def position_m_square(index):
+        return 228 * index, 50
 
     # override
     def proc_event(self, ev, source=None):
         if ev.type == EngineEvTypes.PAINT:
             ev.screen.fill(self._bg_color)
-
             for bt_obj in self._buttons.values():
                 ev.screen.blit(bt_obj.image, bt_obj.rect.topleft)
+            for k in range(1, 4):
+                if k in self._squares:
+                    surf, pos = self._squares[k]
+                    ev.screen.blit(surf, pos)
+
+        elif ev.type == MyEvTypes.MissionStarts:
+            tmp = pygame.Surface((80, 80))
+            tmp.fill((255, 0, 0))
+            adj_pos = list(ChallSelectionView.position_m_square(ev.idx))
+            adj_pos[1] += 30  # y
+            self._squares[ev.idx] = (tmp, adj_pos)
+
+        elif ev.type == MyEvTypes.MissionEnds:
+            self._squares[ev.idx][0].fill((0, 255, 0))
+
+        elif ev.type == MyEvTypes.LootReady:
+            del self._squares[ev.idx]
+            print(ev.obj)
 
         elif ev.type == pygame.MOUSEBUTTONDOWN:
             for bt_obj in self._buttons.values():
                 if bt_obj.rect.collidepoint(ev.pos):
-                    bt_obj.callback()
+                    if bt_obj.callback:
+                        bt_obj.callback()
 
 
 class ChallSelectionCtrl(EventReceiver):
-    def __init__(self):
+    def __init__(self, mod):
         super().__init__()
+        self._modele = mod
+        self._timers = dict()
 
     def proc_event(self, ev, source=None):
-        pass
-        # if ev.type == pygame.MOUSEBUTTONUP:
-        #     self.pev(EngineEvTypes.PUSHSTATE, state_ident=GameStates.Fighting)
+        if ev.type == EngineEvTypes.LOGICUPDATE:
+            if len(self._timers) > 0:
+                defunct_set = set()
+                for mission_idx, v in self._timers.items():
+                    dt = time.time() - v
+                    if dt > 3.0:
+                        self._modele.flag_mission_done(mission_idx)
+                        defunct_set.add(mission_idx)
+                for elt in defunct_set:
+                    del self._timers[elt]
+
+        elif ev.type == MyEvTypes.FightStarts:
+            self.pev(EngineEvTypes.PUSHSTATE, state_ident=GameStates.Fighting)
+
+        elif ev.type == MyEvTypes.MissionStarts:
+            self._timers[ev.idx] = ev.t
 
 
 class ChallSelectionState(BaseGameState):
@@ -100,11 +207,11 @@ class ChallSelectionState(BaseGameState):
             self._avatar.add_xp(random.randint(872, 13125))
 
             game_defs.the_avatar = self._avatar  # shared with other game states
-
-        self.v = ChallSelectionView()
+        self.m = ChallSelectionModel()
+        self.v = ChallSelectionView(self.m)
         self.vavatar = AvatarView(self._avatar)
 
-        self.c = ChallSelectionCtrl()
+        self.c = ChallSelectionCtrl(self.m)
         print(' MainMenuState ENTER')
         self.v.turn_on()
         self.vavatar.turn_on()
--- client/game_events.py
+++ client/game_events.py
@@ -3,5 +3,10 @@ from pkatagames_sdk.engine import enum_for_custom_event_types
 MyEvTypes = enum_for_custom_event_types(
     'PlayerBuysItem',
     'WannaBuySkin',  # deprec
-    'EquipOwnedSkin'  # deprec
+    'EquipOwnedSkin',  # deprec
+
+    'FightStarts',
+    'MissionStarts',  # contains t, idx
+    'MissionEnds',  # contains idx
+    'LootReady'  # contains obj_loot, idx
 )
